766e951ac7b1249ada642c07f0209bb15409d14d
Thomas Lavastida <tlavastida93@gmail.com>
2016-03-01 20:27:29 -0600

    added straight line and turning test codes for arduino

---
+++ arduino/straight_line_test/straight_line_test.ino
@@ -0,0 +1,56 @@
+
+#define STOP 0
+
+#define SPEED 100
+
+
+const int left_speed_pin = 9;
+const int right_speed_pin = 10;
+
+const int left_dir_pin = 7;
+const int right_dir_pin = 8;
+
+const int d2_pin = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  digitalWrite(d2_pin,HIGH);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //go forward
+  digitalWrite(left_dir_pin,HIGH);
+  digitalWrite(right_dir_pin,HIGH);
+  analogWrite(left_speed_pin,SPEED);
+  analogWrite(right_speed_pin,SPEED);
+
+  //5 secs
+  delay(5000);
+
+  //stop
+  analogWrite(left_speed_pin,STOP);
+  analogWrite(right_speed_pin,STOP);
+
+  //2 secs
+  delay(2000);
+
+  //go backward
+  digitalWrite(left_dir_pin,LOW);
+  digitalWrite(right_dir_pin,LOW);
+  analogWrite(left_speed_pin,SPEED);
+  analogWrite(right_speed_pin,SPEED);
+
+  //5 secs
+  delay(5000);
+
+  //stop
+  analogWrite(left_speed_pin,STOP);
+  analogWrite(right_speed_pin,STOP);
+
+
+}
---
+++ arduino/straight_line_test_version_hopeful/straight_line_test_version_hopeful.ino
@@ -0,0 +1,42 @@
+#include "DualMC33926MotorShield.h"
+
+DualMC33926MotorShield md;
+
+#define STOP 0
+
+#define SPEED 150
+
+
+void stopIfFault()
+{
+  if (md.getFault())
+  {
+    Serial.println("fault");
+    while(1);
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  md.init();
+
+  delay(2000);
+  md.setM1Speed(-1*SPEED);
+  delay(10);
+  md.setM2Speed(SPEED);
+
+  delay(3000);
+
+  md.setM1Speed(STOP);
+  delay(10);
+  md.setM2Speed(STOP);
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
