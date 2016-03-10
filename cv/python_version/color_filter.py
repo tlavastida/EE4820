6719437c8b55333af261d94cbe42656d9cb404d7
Thomas Lavastida <tlavastida93@gmail.com>
2016-03-10 00:10:28 +0000

    updated ranges for color detection

--- cv/python_version/color_filter.py
+++ cv/python_version/color_filter.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def brighten(img):
     alpha = 1.5
-    beta = 30 
+    beta = -10 
     h,w,c = img.shape
     zero = np.zeros((h,w,3), np.uint8)
     out = cv2.addWeighted(img,alpha,zero,0,beta)
@@ -28,28 +28,33 @@ def equalize(img):
     return out 
 
 def filterRed(img):
-    lower = np.array([17,15,90],dtype = 'uint8')
-    upper = np.array([70,70,255],dtype = 'uint8')
+    lower = np.array([17,5,87],dtype = 'uint8')
+    upper = np.array([75,75,255],dtype = 'uint8')
     mask = cv2.inRange(img,lower,upper)
     out = cv2.bitwise_and(img,img,mask = mask)
-    return out
+    #return out
+    return (mask,out)
 
 def filterYellow(img):
-    lower = np.array([25,146,190],dtype = 'uint8')
-    upper = np.array([62,200,250],dtype = 'uint8')
+    lower = np.array([10,120,120],dtype = 'uint8')
+    upper = np.array([75,220,220],dtype = 'uint8')
     mask = cv2.inRange(img,lower,upper)
     out = cv2.bitwise_and(img,img,mask = mask)
-    return out
+    #return out
+    return (mask,out)
 
 def detectColor(img):
     bright = brighten(img)
 
-    red = filterRed(bright)
-    yellow = filterYellow(bright)
+    (red,i1) = filterRed(bright)
+    (yellow,i2) = filterYellow(bright)
 
-    totalRed = red.sum(axis=2).sum(axis=1).sum(axis=0)
-    totalYellow = yellow.sum(axis=2).sum(axis=1).sum(axis=0)
+    #totalRed = red.sum(axis=2).sum(axis=1).sum(axis=0)
+    #totalYellow = yellow.sum(axis=2).sum(axis=1).sum(axis=0)
 
+    totalRed = red.sum(axis=1).sum(axis=0)
+    totalYellow = yellow.sum(axis=1).sum(axis=0)
+    
     if totalRed > totalYellow:
         return 'RED'
     else:
@@ -57,9 +62,13 @@ def detectColor(img):
     
 if __name__ == '__main__':
 
-    file1 = 'redvictim.jpg'
-    file2 = 'yellowvictim.jpg'
+    #file1 = 'redvictim.jpg'
+    #file2 = 'yellowvictim.jpg'
 
+    file1 = './imgs/red8.jpg'
+    #file2 = './imgs/red5.jpg'
+    file2 = 'red_test.jpg'
+    
     img1 = cv2.imread(file1)
     img1 = cv2.resize(img1,(320,240))
 
@@ -77,14 +86,14 @@ if __name__ == '__main__':
     cv2.waitKey(0)
 
     
-    red1 = filterRed(bright1)
-    red2 = filterRed(bright2)
+    (red1,i1) = filterRed(bright1)
+    (red2,i2) = filterRed(bright2)
     cv2.imshow('red components: ' + file1,red1)
     cv2.imshow('red components: ' + file2,red2)
     cv2.waitKey(0)
 
-    yellow1 = filterYellow(bright1)
-    yellow2 = filterYellow(bright2)
+    (yellow1,i1) = filterYellow(bright1)
+    (yellow2,i2) = filterYellow(bright2)
     cv2.imshow('yellow components: ' + file1,yellow1)
     cv2.imshow('yellow components: ' + file2,yellow2)
     cv2.waitKey(0)
