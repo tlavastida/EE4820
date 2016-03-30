abc65eabf477aed45867467289558c22435d7d9d
Thomas Lavastida <tlavastida93@gmail.com>
2016-03-30 02:12:49 +0000

    updated robot_control, robot_serial

--- raspi/robot/robot_control.py
+++ raspi/robot/robot_control.py
@@ -6,24 +6,94 @@
 #Thomas Lavastida
 #March 9, 2016
 
+import robot_serial
 import numpy
 
 
-#There are four victims we need to rescue
-total_victims = 4
+class Robot:
+    #constructor
+    def __init__(self):
+
+        ### Serial Stuff ###
+        self.portname = "\dev\ttyACM0"
+        self.baud = 250000
+        self.timeout = 2
+        self.ser = robot_serial.RobotSerial(self.portname,self.baud,self.timeout)
+        ####################
+
+        ### There are four total victims ###
+        self.total_victims = 4
+	self.found_victims = 0
+        ####################
+
+        ### State variables ###
+        self.grid_x = 0
+        self.grid_y = 0
+        #insert variable/s for encoder measurements maybe?
+	self.dx = 1
+        self.dy = 0
+
+    #updates grid positions in the map using an encoder reading
+    def update_grid_position(self,encoder1,encoder2):
+        avg = (encoder1+encoder2)//2 #integer division for now, maybe try floating point plus round later
+        self.grid_x += self.dx*avg   #FIX LATER - NEED CORRECT CONVERSION FACTOR TO GRID SPACE
+        self.grid_y += self.dy*avg   #FIX LATER - NEED CORRECT CONVERSION FACTOR TO GRID SPACE
+
+    def forward(self,distance):
+        self.ser.send('G'+str(distance))
+        while self.ser.available() <= 0:
+
+        
+        msg = self.ser.recv()
+        return msg #PLACEHOLDER
+        
+    #tells arduino to turn left
+    def turn_left(self):
+        self.ser.send('T1L')
+        while self.ser.available() <= 0:
+
+        msg = self.ser.recv()
+        self.dx = -1*dy
+        self.dy = dx
+        return msg #PLACEHOLDER
+
+    #tells arduino to turn right
+    def turn_right(self):
+        self.ser.send('T1R')
+        while self.ser.available() <= 0:
+
+        msg = self.ser.recv()
+        self.dx = dy
+        self.dy = -1*dx
+        return msg #PLACEHOLDER
+
 
 
 def main_loop():
-    #initialization
-    num_rescued = 0
-    current_pos = np.array([0,0])
+
+    sara = Robot()
     
-    #other initialiation....
+    text = input()
+    while text != 'exit':
+        words = text.split()
+        cmd = words[0]
+        if cmd == 'go':
+            if not words[1]:
+                distance = 100
+            else:
+                distance = int(words[1])
+            msg = sara.forward(distance)
+            print(msg)
+        elif cmd == 'left':
+            msg = sara.turn_left()
+            print(msg)
+        elif cmd == 'right':
+            msg = sara.turn_right()
+            print(msg)
+        else:
+            print('Not a valid command')
 
-    while num_rescued != total_victims:
-
-        #get a path
-        path = getPath()
+        txt = input()
 
         
 
--- raspi/robot/robot_serial.py
+++ raspi/robot/robot_serial.py
@@ -9,15 +9,18 @@ class RobotSerial:
     #     self.timeout = 1
     #     self.port = serial.Serial(portname,baud_rate,timeout)
 
+    #constructor
     def __init__(self,portname = "/dev/ttyACM0",baud_rate = 115200,timeout = 1):
         self.portname = portname
         self.baud_rate = baud_rate
         self.timeout = timeout
         self.port = serial.Serial(portname,baud_rate,timeout=timeout)
 
+    #destructor
     def __del__(self):
         self.port.close()
 
+    #sends data to device
     def send(self,msg):
         if type(msg) is bytes:
             self.port.write(msg)
@@ -26,9 +29,14 @@ class RobotSerial:
         else:
             raise TypeError('msg needs to be of type bytes or str')
 
+    #receives data from device
     def recv(self):
         return self.port.readline()
 
+    #returns number of bytes in recv buffer
+    def available(self):
+        return self.port.in_waiting()
+    
     def name(self):
         return self.port.name
 
